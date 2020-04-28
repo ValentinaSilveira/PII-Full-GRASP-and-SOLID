31e144a903db2263e6d8a23beed174ff3c895882
Valentina Silveira <62141464+ValentinaSilveira@users.noreply.github.com>
2020-04-28 18:30:35 -0300

    Update IPrinter.cs

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -1,10 +1,10 @@
 using System;
-// Se borró la clase AllInOnePrinter dado que era código redundante con los cambios realizados.
-// Se implementó que el método PrintRecipe sea polimórfico para imprimir por consola o archivo.
-// Se creó la interfase IPrinter dado que se establece un tipo en común para la impresión
+
+// Implemente que el método PrintRecipe sea polimórfico para imprimir por consola o archivo.
+// Cree la interfase IPrinter dado que se establece un tipo en común para la impresión
 // en cualquiera de las dos clases.
 
-// Se utilizó el patrón Polymorphism. Ambos tipos de clases no interfieren, de manera que
+// Utilice el patrón Polymorphism. Ambos tipos de clases no interfieren, de manera que
 // no dan resultados inesperados. Se cumple el principio de sustitución de Liskov.
 
 
