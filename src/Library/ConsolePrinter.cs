562925c93d150a36f5c2dc2bdf52e9bac31c26e5
Valentina Silveira <62141464+ValentinaSilveira@users.noreply.github.com>
2020-04-28 18:28:33 -0300

    Update ConsolePrinter.cs

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -1,10 +1,10 @@
 using System;
 
-// Aquí se hace una de las implementaciones del tipo IPrinter definido en la interfase.
+// Lo que hice fue hacer uuna de las implementaciones del tipo IPrinter definido en la interfase.
 // Se recibe una receta como parámetro y se implementa el método PrintRecipe para 
 // escribir en pantalla.
 
-// Comentarios en IPrinter explican cómo se trabajó.
+// Los comentarios en IPrinter explican cómo se trabajó.
 namespace Full_GRASP_And_SOLID.Library
 {
     public class ConsolePrinter : IPrinter
